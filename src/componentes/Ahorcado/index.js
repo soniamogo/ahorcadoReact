b7a565b717447a86941b6772bb1d462258bbd48c
soniamogo <soniamogo@gmail.com>
2022-07-20 20:06:36 +0200

    inicio

---
+++ src/componentes/Ahorcado/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Ahorcado extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+    render() {
+        return (
+            <div>Ahorcado</div>
+        )
+    }
+}
